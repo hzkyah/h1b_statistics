5a4c6fae7a1ce70e3b1593655973c97e9cc609a2
hzkyah <44174716+hzkyah@users.noreply.github.com>
2018-10-31 11:44:11 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-python3 ./src/h1b_counting.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+python3 ./src/h1b_counting.py ./src/config.txt ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
